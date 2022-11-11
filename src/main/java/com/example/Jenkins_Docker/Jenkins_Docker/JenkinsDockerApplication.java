fc2bfa898d0fd673ecf510bb535d393e297df2f4
Ajinkya Pethkar <ajinkyapethkar@gmail.com>
2022-11-11 12:02:38 +0530

    Commit

---
+++ src/main/java/com/example/Jenkins_Docker/Jenkins_Docker/JenkinsDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.Jenkins_Docker.Jenkins_Docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Jenkins_Docker/Jenkins_Docker/JenkinsDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Jenkins_Docker.Jenkins_Docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
